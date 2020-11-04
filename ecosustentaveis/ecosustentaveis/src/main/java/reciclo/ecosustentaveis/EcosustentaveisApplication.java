cf90eb1f7180338f0046279e831bc28bd223e114
unknown <jacquelinee.barbosa@hotmail.com>
2020-11-04 18:30:31 -0300

    ProjetoIntegrador

---
+++ ecosustentaveis/ecosustentaveis/src/main/java/reciclo/ecosustentaveis/EcosustentaveisApplication.java
@@ -0,0 +1,13 @@
+package reciclo.ecosustentaveis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcosustentaveisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcosustentaveisApplication.class, args);
+	}
+
+}
---
+++ ecosustentaveis/ecosustentaveis/src/test/java/reciclo/ecosustentaveis/EcosustentaveisApplicationTests.java
@@ -0,0 +1,13 @@
+package reciclo.ecosustentaveis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcosustentaveisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
